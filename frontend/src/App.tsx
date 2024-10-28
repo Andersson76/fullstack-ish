66c854f7fded4dd2e720c4726b411ca7d3550de8
Martin Andersson <martin.andersson@me.com>
2024-10-28 10:59:58 +0100

    Second commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import "./App.css";
 import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -26,19 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <p>{message && `Hello ${message}!`}</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{message && `Hello ${message}!`}</h1>
     </>
   );
 }
